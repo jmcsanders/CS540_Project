57daef62cbde8092873f14ba4d4f08aa873b562f
Nighthamster <nighthamster@gmail.com>
2022-02-28 20:42:50 -0500

    airlines

--- Pyhtoncode.py
+++ Pyhtoncode.py
@@ -1,12 +1,12 @@
-#Exemple of plotting a data chart with python
+#Example of plotting a data chart with python
 #Importing libraries
 from matplotlib import pyplot as plt
 import numpy as np
  
 # Creating dataset
-flight = ['AUDI', 'BMW', 'FORD',
-        'TESLA', 'JAGUAR', 'MERCEDES']
- 
+flight = ['AA', 'SW', 'DL',
+        'UA', 'LH', 'RA']
+ #AA= American Airlines, SW= SouthWest Airlines DL=Delta Airlines, UA=United Airlines, LH=Lufthansa, RA=Royal Nepal
 data = [23, 17, 35, 29, 12, 41]
  
 # Creating plot
