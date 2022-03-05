2cdd21e94358241ee1e28e5a635120c478821e41
MartiT52 <98545089+MartiT52@users.noreply.github.com>
2022-03-05 07:16:06 -0500

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,109 @@
+from sqlite3 import Row
+import pandas as pd
+import mysql.connector as msql
+from mysql.connector import Error
+
+#
+# Connect to MySQL Workbench local
+#
+try:
+    conn = msql.connect(host='localhost', user='root',  
+                        password='P4he7qv*', database='FlightData')
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Connected to database: ", record)
+
+
+        #
+        # Average Delay Time by Airline
+        # Hard coded in this case to 'WN' for Southwest Airlines for testing
+        #
+        # Selects the average from the delay time from entires that are more than 1, indicating a delay
+        #
+        cursor.execute("SELECT avg(DEP_DELAY_NEW) FROM dataset WHERE MKT_UNIQUE_CARRIER = 'WN' AND DEP_DELAY_NEW > 0")
+        query = (cursor.fetchone()[0])
+        print(query)
+
+
+        #
+        # Chance of Delay by Airline
+        # Hard coded in this case to 'WN' for Southwest Airlines for testing
+        # 
+        # Selects the count of entires that are delayed and on time to calculate the chance of a delay for a certain airline
+        # Multiplies result by 100 to represent a percentage
+        #
+        cursor.execute("SELECT count(DEP_DELAY_NEW) FROM dataset WHERE MKT_UNIQUE_CARRIER = 'WN' AND DEP_DELAY_NEW = 0")
+        on_time = (cursor.fetchone()[0])
+        cursor.execute("SELECT count(DEP_DELAY_NEW) FROM dataset WHERE MKT_UNIQUE_CARRIER = 'WN' AND DEP_DELAY_NEW > 0")
+        delayed = (cursor.fetchone()[0])
+
+        result = (delayed / (on_time + delayed)) * 100
+        print(result)
+
+
+        #
+        # Chance of cancelation by Airline
+        # Hard coded in this case to 'WN' for Southwest Airlines for testing
+        # 
+        # Selects the count of entires that are cancelled and not cancelled to calculate the chance of a cancellation for a certain airline
+        # Multiplies result by 100 to represent a percentage
+        #
+        cursor.execute("SELECT count(CANCELLED) FROM dataset WHERE MKT_UNIQUE_CARRIER = 'WN' AND CANCELLED = 0")
+        on_time = (cursor.fetchone()[0])
+        cursor.execute("SELECT count(CANCELLED) FROM dataset WHERE MKT_UNIQUE_CARRIER = 'WN' AND CANCELLED = 1")
+        delayed = (cursor.fetchone()[0])
+
+        result = (delayed / (on_time + delayed)) * 100
+        print(result)        
+
+
+        #
+        # Average Delay Time by Departure Airport
+        # Hard coded in this case to 'DEN' for Denver for testing
+        #
+        # Selects the average from the delay time from entires that are more than 1, indicating a delay
+        #
+        cursor.execute("SELECT avg(DEP_DELAY_NEW) FROM dataset WHERE ORIGIN = 'DAL' AND DEP_DELAY_NEW > 0")
+        query = (cursor.fetchone()[0])
+        print(query)
+
+
+        #
+        # Chance of Delay by Departure Airport
+        # Hard coded in this case to 'DEN' for Denver for testing
+        #
+        # Selects the count of entires that are delayed and on time to calculate the chance of a delay for a certain departure airport
+        # Multiplies result by 100 to represent a percentage
+        #
+        cursor.execute("SELECT count(DEP_DELAY_NEW) FROM dataset WHERE ORIGIN = 'DAL' AND DEP_DELAY_NEW = 0")
+        on_time = (cursor.fetchone()[0])
+        cursor.execute("SELECT count(DEP_DELAY_NEW) FROM dataset WHERE ORIGIN = 'DAL' AND DEP_DELAY_NEW > 0")
+        delayed = (cursor.fetchone()[0])
+
+        result = (delayed / (on_time + delayed)) * 100
+        print(result)
+
+
+        #
+        # Chance of cancelation by Deparure Airport
+        # Hard coded in this case to 'DEN' for Denver for testing
+        # 
+        # Selects the count of entires that are cancelled and not cancelled to calculate the chance of a cancellation for a certain departure airport
+        # Multiplies result by 100 to represent a percentage
+        #
+        cursor.execute("SELECT count(CANCELLED) FROM dataset WHERE ORIGIN = 'DEN' AND CANCELLED = 0")
+        on_time = (cursor.fetchone()[0])
+        cursor.execute("SELECT count(CANCELLED) FROM dataset WHERE ORIGIN = 'DEN' AND CANCELLED = 1")
+        delayed = (cursor.fetchone()[0])
+
+        result = (delayed / (on_time + delayed)) * 100
+        print(result)   
+
+
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+
